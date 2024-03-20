d0b6c2ce6927ca1c8d526bfd33bb37f31047f8d0
parzival-watts <dylan.philliplitz@gmail.com>
2024-03-20 13:24:33 -0300

    add enums

---
+++ src/main/java/io/github/parzival/authapi/enums/RoleEnum.java
@@ -0,0 +1,12 @@
+package io.github.parzival.authapi.enums;
+
+public enum RoleEnum {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    RoleEnum(String role) {
+        this.role = role;
+    }
+}
